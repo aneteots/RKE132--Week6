691f5e6e205f94181fe2fcae3e5c29bddc575f58
Anete Ots <anete.ots@tktk.ee>
2024-03-23 15:19:05 +0200

    add week6 code

---
+++ wwk6StringsAndArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+string hello = "hello, world!";
+
+int stringLenght = hello.Length;
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+hello = hello.Replace(hello[0], char.ToUpper(hello[0]));
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+//int wordCounter = 0;
+//for (int i = 0; i < hello.Length; i++)
+//{
+//  Console.WriteLine($"{i} symbol in string: {hello [i]}");
+//}
+
+//Console.WriteLine(stringLenght);
+
+
